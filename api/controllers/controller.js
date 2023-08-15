6e8628e0561582d0ba29002c983f7113303a414b
kevin.jimenez <kevin.jimenez@vasslatam.com>
2023-08-15 18:43:05 -0500

    Avance numero uno en pokeapi

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,11 +1,29 @@
 
-export function saludar(){
-    console.log("hola soy kevin")
+export async function buscarPokemon() {
+    let data = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    let dataParseada = await data.json();
+    console.log(dataParseada);
+
+    let arrayDePokemones = [];
+    for (let i = 0; i < dataParseada.results.length; i++) {
+        const pokemon = dataParseada.results[i];
+
+        //console.log("i = ", i);
+        //console.log("Pokemon que estamos interando: ", pokemon.url)
+        let pokemonData = await fetch(pokemon.url);
+        let pokemonParseado = await pokemonData.json();
+
+        let pokemonFormat = {
+            id: pokemonParseado.id,
+            nombre: pokemonParseado.name,
+            tipo: pokemonParseado.types,
+            imagen: pokemonParseado.sprites.other.dream_world.front_default
+
+
+        }
+        arrayDePokemones.push(pokemonFormat)
+    }
+    return arrayDePokemones;
 }
 
-export async function buscarPokemon(){
-    let data = await fetch("https://pokeapi.co/api/v2/pokemon");
-    let newData = await data.json();
-    let newDataxx = newData.results[0]
-    console.log("data", newDataxx)
-}
+buscarPokemon();
--- api/main.js
+++ api/main.js
@@ -1,5 +1,19 @@
-import { buscarPokemon, saludar  } from "./controllers/controller.js";
+import { buscarPokemon } from "./controllers/controller.js";
+let root = document.getElementById("root")
 
-console.log( "soy saludar ",  saludar())
+async function render(){
+    let pokemons = await buscarPokemon();
+    console.log("Estoy en la funcion de busqueda: ", pokemons)
+    let html = "";
+    pokemons.forEach((pokemon) => {
+        let mappPokemons = `<div class="card">
+                                <span>${pokemon.nombre}</span>
+                                <span>${pokemon.id}</span>
+                                <img class="card-image" src="${pokemon.imagen}">
+                            </div>` 
+        html = root.innerHTML += mappPokemons;
+    });
 
-buscarPokemon();
+}
+render();
+//buscarPokemon();
